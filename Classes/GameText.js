0968821e518cf938889c63781227ef42c0f94a3d
MrElektronix <milovanpelt@gmail.com>
2017-06-22 21:48:34 +0200

    update
    - player moves forward if word correctly typed
    - added node js

--- Classes/GameText.js
+++ Classes/GameText.js
@@ -1,12 +1,12 @@
 class GameText {
-  constructor(context) {
+  constructor(canvas, context) {
     this.context = context;
   }
 
   Text(message) {
     this.context.beginPath();
     this.context.font = "30px Arial";
-    this.context.fillText(message || " ", 100, 100);
+    this.context.fillText(message || " ", (canvas.width / 2) - 80, canvas.height - 100);
   }
 
 
---
+++ Milo/app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var users = [];
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+io.on('connect', (socket) => {
+  console.log("a user connected");
+  socket.on('setUsername', (data) => {
+    if (users.indexOf(data) > -1) {
+      socket.emit('userExists', data + ' username is taken!');
+    } else {
+      users.push(data);
+      socket.emit('userSet', {
+        username: data
+      });
+    }
+  })
+  socket.on('msg', (data) => {
+    //send message to everyone
+    io.emit('newmsg', data);
+  })
+});
+
+server.listen(3000, () => {
+  console.log("listening on port 3000");
+});
